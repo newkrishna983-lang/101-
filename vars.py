a41947f69cbf02ddca39c8e469c81c7e11b992c0
newkrishna983-lang <newkrishna983@gmail.com>
2026-05-29 12:08:47 +0530

    Update API token and clean up configuration

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "34422904"))
 API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8753577119:AAGA9qdWdgCYAPKeNPZRuaYR9s9ClTfvWOg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8121767231:AAHOM-Dm3jh101boHkqBfpaKlwF_efucGB4")
 
 CREDIT = os.environ.get("CREDIT", "•Ɱའ★Krΐຮhήส•❤️‍🔥")
 # MongoDB Configuration
